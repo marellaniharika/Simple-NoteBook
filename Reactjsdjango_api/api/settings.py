1296a8fac093ec43d2408e4299db834083f85a44
marellaniharika <marellaniharika799@gmail.com>
2024-07-20 13:40:46 -0500

    allowallorigin

--- Reactjsdjango_api/api/settings.py
+++ Reactjsdjango_api/api/settings.py
@@ -54,10 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'api.urls'
 
